95eac96152b894d3bee448f988c0bdfe476acfaf
NetFeez <netfeez.dev@gmail.com>
2026-05-05 05:14:49 +0000

    feat: add Isomorphic export to index.ts for improved module accessibility

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { VIO, VIO as default } from "./VIO.js";
 export { VIOStrict } from "./strict/VIOStrict.js";
 export { VIOError } from "./VIOError.js";
 export { Frame } from "./Frame.js";
+export { Isomorphic } from "./support/Isomorphic.js";
